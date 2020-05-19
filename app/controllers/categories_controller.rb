2d4feefde8c115cc49b65b8fefd03ca4de5e6adf
esthercsoke <esthercsoke@gmail.com>
2020-05-19 10:33:24 +1000

    changes to navbar look

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,4 @@
-class CategoriesController < ApplicationController
+eaiclass CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
   
     def index
